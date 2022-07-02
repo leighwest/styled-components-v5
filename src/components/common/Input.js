fd13dd8d708c775961b45358f005d629dc5ef98d
Leigh West <leighwest@hotmail.com>
2022-07-02 08:41:49 +0000

    Added username and password fields

---
+++ src/components/common/Input.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  padding: 4px 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 1em;
+  font-family: 'Open Sans';
+  margin-bottom: 8px;
+  width: 100%;
+  box-sizing: border-box;
+  height: 40px;
+  &:focus {
+    outline: none;
+    border: 1px solid #fcba03;
+  }
+`
+
+export { Input }
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,51 @@
+import { useState } from "react";
+
 import { PageLayout } from "components/common"
+import styled from 'styled-components';
+import { Input } from "components/common";
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 400px;
+  background: white;
+  border: 1px solid #eee;
+  padding: 16px;
+  box-sizing: border-box;
+  color: black;
+  border-radius: 4px;
+`
 
 
 const Login = () => {
+  const [formFields, setFormFields] = useState({ username: '', password: '' });
+
+  const handleInputChange = e => {
+    e.persist();
+    setFormFields(s => ({
+      ...s,
+      [e.target.name]: e.target.value
+    }))
+  }
+
   return (
     <PageLayout>
-      Login
+      <h1>
+        Login
+      </h1>
+      <Form>
+        <Input
+          value={formFields.username}
+          onChange={handleInputChange}
+          name="username"
+          type="text"
+          placeholder="Username" />
+        <Input
+          value={formFields.password}
+          onChange={handleInputChange}
+          name="password"
+          type="password"
+          placeholder="Password" />
+      </Form>
     </PageLayout>
   )
 }
